8703c1ed4796d9472b42d6c9f92fca655c6fa29f
Arthgar25 <desarthgar@gmail.com>
2025-09-04 13:25:00 -0700

    Se implemento el primer ejercicio de los encargados

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -2,8 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
+char obtenerPaloAleatorio();
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    char elPalo = obtenerPaloAleatorio();
+    printf("Palo aleatorio: %c\n", elPalo);
     return 0;
+}
+
+char obtenerPaloAleatorio(){
+    srand(time(NULL));
+    int valor = 1;
+    char palo;
+    valor = (rand()%4+1);
+
+    switch(valor){
+        case 0: 
+            palo = 'P';
+            break;
+        case 1: 
+            palo = 'T'; 
+            break;
+        case 2: 
+            palo = 'D'; 
+            break;
+        case 3: 
+            palo = 'C'; 
+            break;
+        default:
+            break;
+    }
+        
+    return palo;
 }
